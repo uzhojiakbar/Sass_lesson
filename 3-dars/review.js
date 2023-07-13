3663ff60344408142006b90f77c8804e8eedd833
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-07-14 00:41:09 +0500

    Proporties, Nesting Proporties, shorthand

---
+++ 3-dars/review.js
@@ -0,0 +1,36 @@
+// ===================
+// Proporties (Xossalar)
+// Boshlangich xossalar css niki bn bir xil
+
+    // .circle{
+    //     color: red;
+    //     width: 400px;
+    //     height: 400px;
+    //     display: flex;
+    //     justify - content: center;
+    //     align - items: center;
+    // }
+
+// ===================
+// Nesting Proporties (ichma ich xossalar)
+// Nesting Proporties orqali biz boshlangich sozi bir xil bolgan xossalarni nesting qilishimiz mumkin, masalan:
+
+    // .heading{
+    //     font: {
+    //         size: 40px;
+    //         family: 'Times New Roman', Times, serif;
+    //         weight: 900;
+    //     }
+    // }
+
+
+// ===================
+// Shorthands (Qisqartimalar)
+    // .info - page{
+    //     margin: auto {
+    //         bottom: 45px;
+    //         left: 25px;
+    //     }
+    // }
+
+// ===================
