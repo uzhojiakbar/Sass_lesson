62b2c5ef763e5204a9a0fa834dfa27c79c2f94d2
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-07-14 00:11:56 +0500

    install Sass, Compiler, Sass and Scss

---
+++ 1-dars/review.js
@@ -0,0 +1,22 @@
+// Birinchi orinda Sass ni kompyuterga o'rnatamiz
+// Buning uchun terminalga "npm i sass " ni yozamiz
+// agar global ornatmoqchi bolsak "npm i -g sass"
+
+// Agar Global ornatsak har doim terminalga sass ni ornatmaymiz, boshida 1 ta ornatamiz va har doim ishlaydi
+
+// Keyin  VS Code Expetsions dan Live "Live Sass Compiler ni o'rnatamiz"
+
+// Endi 1 ra sass yoki scss fayl ochib ishlaymiz va code ni saxranit qiling pastgai watching sass ni bosamiz
+
+// ===========================
+// Sass bn Scss ni farqi nimada ?
+// 1. agar .sass fayl yaratadigan bolsa codelarni bunday yozamiz
+    // $color:red
+    // body
+    //     color: $color
+// 2. Agar scss yozadigan bolsak codlarni mana bunday xolatda yozamiz
+    // $color:red;
+    // body{
+    //     color: $color
+    // }
+// Bularni yozilishida farqi shunday, lekin saxranit qilgandan keyin css ga qayta compiler bolganda bir xil xolatda compiler qilib beradi
