10d40ce7ec84a7e2a7e850a9b86c6da1eceba553
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-07-14 19:00:28 +0500

    Lists

---
+++ 6-dars/review.js
@@ -0,0 +1,12 @@
+// =======================
+// Lists {Royxatlar}
+// Lists - oz ichida bir necha qiymatlarni saqlaydi. Royxatlarni bosh joy yoki vergullar bilan ajratamiz
+
+
+    // $margin: 40px auto;
+    // $box-shadow: 10px 20px 0 red;
+
+    // .box{
+    //     margin: $margin;
+    //     box-shadow: $box-shadow;
+    // }
