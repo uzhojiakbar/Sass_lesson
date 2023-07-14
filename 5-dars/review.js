5c47d30aef567c4ef0b596be76bfedc9939a477c
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-07-14 18:52:17 +0500

    Arithmetic operations

---
+++ 5-dars/review.js
@@ -0,0 +1,20 @@
+// SASS barcha matematik ammalarni qo'llab quvvatlaydi!
+
+// ==================
+// Arifmetik ammalar
+// + -> qoshuv
+// - -> ayruv
+// * -> kopayturuv
+// / -> boluv
+// % -> bolingandagi qoldiq
+
+// ==================
+// @debug - bu kalit soz faqatgina xisobni ekranga chiqarish uchun ishlatilad
+
+// ==================
+    // $middle - font - size: 24px;
+
+    // h1{
+    //     font - size: $middle - font - size + 3;
+    // }
+
